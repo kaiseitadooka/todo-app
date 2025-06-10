5ad44d123295f453cba2e6453f6874b6e5f6325f
kaiseitadooka <kaiseitadooka@gmail.com>
2025-06-10 04:09:23 +0000

    first commit

---
+++ message/src/main/java/com/example/message/repository/MessageRepository.java
@@ -0,0 +1,7 @@
+package com.example.message.repository;
+
+import com.example.message.model.Message;
+import org.springframework.data.jpa.repository.JpaRepository;
+public interface MessageRepository extends JpaRepository<Message,Integer> {
+    
+}
